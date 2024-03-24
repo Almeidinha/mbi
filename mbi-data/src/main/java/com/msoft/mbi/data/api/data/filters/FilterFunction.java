b915cfb6495d13562b45fa1ff1c25e10346cdc16
Marcos Almeida <blackheal@gmail.com>
2024-03-24 20:16:51 -0300

    remove unnecessary code

--- mbi-data/src/main/java/com/msoft/mbi/data/api/data/filters/FilterFunction.java
+++ mbi-data/src/main/java/com/msoft/mbi/data/api/data/filters/FilterFunction.java
@@ -4,26 +4,26 @@ import com.msoft.mbi.data.api.data.indicator.Operator;
 
 public interface FilterFunction {
 
-    public static final int SEQUENCE_FILTER = 1;
-    public static final int ACCUMULATED_FILTER = 2;
+    int SEQUENCE_FILTER = 1;
+    int ACCUMULATED_FILTER = 2;
 
-    public boolean verifyCondition(double originalValue);
+    boolean verifyCondition(double originalValue);
 
-    public void setOperator(Operator operator);
+    void setOperator(Operator operator);
 
-    public Operator getOperator();
+    Operator getOperator();
 
-    public void setValue(String valor);
+    void setValue(String valor);
 
-    public String getValue();
+    String getValue();
 
     String getFormattedValue();
 
-    public int getType();
+    int getType();
 
-    public String getFieldName();
+    String getFieldName();
 
-    public String getFieldTitle();
+    String getFieldTitle();
 
-    public Object clone();
+    Object clone();
 }
